818ac8bf64728c740bd11cebf8d51b5a606f7e41
Patrick <83720283+patrickzhou1234@users.noreply.github.com>
2022-12-01 13:24:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Google_Docs_Ontask
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://docs.google.com/document/d/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
 setInterval(() => {
     document.querySelectorAll(".docs-title-save-label-text-link")[0].innerHTML="Last edit was seconds ago";
 }, 3000);
